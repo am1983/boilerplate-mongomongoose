5b425e79badf65bf4f84181ba8e4281dec5f16a0
Aaron Marks <me@aaronmarks.dev>
2021-03-12 15:53:39 -0500

    queryChain

--- myApp.js
+++ myApp.js
@@ -115,7 +115,16 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person
+  .find({favoriteFoods:foodToSearch})
+  .sort({name : "asc"})
+  .limit(2)
+  .select("-age")
+  .exec((err, data) => {
+    if(err)
+      done(err);
+   done(null, data);
+ })
 };
 
 /** **Well Done !!**
